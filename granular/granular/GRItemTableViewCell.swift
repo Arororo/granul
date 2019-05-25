bb9428460afadbc0f7b18b9031dbc99305f1aafb
Dmitry Babenko <dmitry.babenko@aequilibrium.ca>
2019-05-25 16:40:11 -0700

    Add custom cell UI

--- granular/granular/GRItemTableViewCell.swift
+++ granular/granular/GRItemTableViewCell.swift
@@ -9,21 +9,24 @@
 import UIKit
 
 class GRItemTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var itemIcon: UIImageView!
+    @IBOutlet weak var iconActivityIndicator: UIActivityIndicatorView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.imageView?.backgroundColor = .gray
-        self.imageView?.contentMode = .scaleAspectFit
-        self.imageView?.image = nil
+        self.itemIcon.image = nil
+        self.nameLabel.text = nil
     }
     
     func configure(with item: GRItemModel) {
-        self.textLabel?.text = item.name
+        self.nameLabel.text = item.name
         if let imgPath = item.url {
             let imageUrl = GRNetworkManager.shared.iconUrlString(for: imgPath)
-            self.imageView?.loadImageUsingCache(withUrl: imageUrl, completion: { isSuccess in
+            self.iconActivityIndicator.startAnimating()
+            self.itemIcon.loadImageUsingCache(withUrl: imageUrl, completion: { isSuccess in
                 DispatchQueue.main.async { [weak self] in
-                    self?.contentView.setNeedsLayout()
+                    self?.iconActivityIndicator.stopAnimating()
                 }
             })
         }
